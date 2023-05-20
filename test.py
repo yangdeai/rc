a4a072bb2086b04e86caa5f415132fb22f32e877
yangdeai <yangdeai@126.com>
2023-05-20 20:15:35 +0800

    first commit file

---
+++ test.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+==================================================
+@Project:        rcProject
+@Author:         yang deai
+@Time:           2023/5/15:9:54
+@File:           test_func.py
+==================================================
+"""
+
+# 导入常用包
+import os
+from datetime import datetime
+import logging
+import time
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import transforms
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets
+
+from models.resnet import resnet101
+
+
+# 超参数定义
+# 批次的大小
+batch_size = 64  # 可选32、64、128
+
+test_mean, test_std = (0.4942, 0.4851, 0.4504), (0.2467, 0.24295, 0.2616)
+test_data_transform = transforms.Compose([
+                       transforms.ToTensor(),
+                       # transforms.Normalize(test_mean, test_std)
+                      ])
+
+test_cifar_dataset = datasets.CIFAR10('cifar10', train=False, download=False, transform=test_data_transform)
+test_loader = torch.utils.data.DataLoader(test_cifar_dataset,
+                                          batch_size=batch_size, num_workers=4,
+                                          shuffle=False)
+
+exp_name = 'exp0'
+weight_dir = './weights'
+weight_dir = weight_dir + f'/resnet101_{exp_name}.pth'
+
+log_dir = f'./runs/test/{exp_name}'
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+logging.basicConfig(filename=log_dir + '.txt',
+                    filemode='a',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s-%(funcName)s',
+                    level=logging.INFO)
+# writer
+writer = SummaryWriter(log_dir)
+# Set fixed random number seed
+torch.manual_seed(42)
+# device
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+criterion = torch.nn.CrossEntropyLoss()
+
+
+@torch.no_grad()
+def test():
+    # 加载模型权重
+    model = resnet101()
+    model = model.to(device)
+    model.load_state_dict(torch.load(weight_dir))
+
+    model.eval()
+    test_total_correct = 0
+    test_total_loss = 0
+    test_total_num = 0
+    start = time.time()
+    for iter, (images, labels) in enumerate(test_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        test_total_loss += loss.item()
+        test_total_correct += (outputs.argmax(1) == labels).sum().item()
+        test_total_num += labels.shape[0]
+        # Write accuracy
+        writer.add_scalar('test acc', test_total_correct / test_total_num * 100, iter)
+        writer.add_scalar('test loss', test_total_loss / test_total_num * 100, iter)
+
+    logging.info("test_loss:{:.4f}, test_acc:{:.4f}%".format(test_total_loss / test_total_num * 100,
+                                                             test_total_correct / test_total_num * 100))
+    logging.info("test_time:{:.4f}s".format(time.time() - start))
+
+
+if __name__ == "__main__":
+    logging.info("===   !!!START TESTING!!!   ===")
+    logging.info("test data num:{}".format(len(test_cifar_dataset)))
+    test()
+    logging.info("===   !!! END TESTING !!!   ===")
+    logging.info("\n\n\n\n")
---
+++ utils.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+==================================================
+@Project:        rcProject
+@Author:         yang deai
+@Time:           2023/5/15:9:54
+@File:           utils.py
+==================================================
+"""
+
+import os
+import sys
+import re
+import datetime
+
+import numpy
+
+import torch
+from torch.optim.lr_scheduler import _LRScheduler
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+
+def get_network(args):
+    """ return given network
+    """
+
+    if args.net == 'vgg16':
+        from models.resnet import resnet101
+        net = resnet101()
+
+    else:
+        print('the network name you have entered is not supported yet')
+        sys.exit()
+
+    if args.gpu:  # use_gpu
+        net = net.cuda()
+
+    return net
+
+def get_training_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=True):
+    """ return training dataloader
+    Args:
+        mean: mean of cifar10 training dataset
+        std: std of cifar10 training dataset
+        path: path to cifar10 training python dataset
+        batch_size: dataloader batchsize
+        num_workers: dataloader num_works
+        shuffle: whether to shuffle
+    Returns: train_data_loader:torch dataloader object
+    """
+
+    transform_train = transforms.Compose([
+        #transforms.ToPILImage(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+    cifar10_training = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    cifar10_training_loader = DataLoader(
+        cifar10_training, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+
+    return cifar10_training_loader
+
+def get_test_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=True):
+    """ return training dataloader
+    Args:
+        mean: mean of cifar10 test_func dataset
+        std: std of cifar10 test_func dataset
+        path: path to cifar10 test_func python dataset
+        batch_size: dataloader batchsize
+        num_workers: dataloader num_works
+        shuffle: whether to shuffle
+    Returns: cifar10_test_loader:torch dataloader object
+    """
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+    cifar10_test = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    cifar10_test_loader = DataLoader(
+        cifar10_test, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+
+    return cifar10_test_loader
+
+def compute_mean_std(dataset):
+    """compute the mean and std of cifar10 dataset
+    Args:
+        cifar10_training_dataset or cifar10_test_dataset
+        witch derived from class torch.utils.data
+
+    Returns:
+        a tuple contains mean, std value of entire dataset
+    """
+
+    mean_r = numpy.mean(dataset.data[:, :, :, 0]) / 255.0
+    mean_g = numpy.mean(dataset.data[:, :, :, 1]) / 255.0
+    mean_b = numpy.mean(dataset.data[:, :, :, 2]) / 255.0
+
+    std_r = numpy.std(dataset.data[:, :, :, 0]) / 255.0
+    std_g = numpy.std(dataset.data[:, :, :, 1]) / 255.0
+    std_b = numpy.std(dataset.data[:, :, :, 2]) / 255.0
+
+    mean = mean_r, mean_g, mean_b
+    std = std_r, std_g, std_b
+
+    return mean, std
+
+
+class WarmUpLR(_LRScheduler):
+    """warmup_training learning rate scheduler
+    Args:
+        optimizer: optimzier(e.g. SGD)
+        total_iters: totoal_iters of warmup phase
+    """
+    def __init__(self, optimizer, total_iters, last_epoch=-1):
+
+        self.total_iters = total_iters
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        """we will use the first m batches, and set the learning
+        rate to base_lr * m / total_iters
+        """
+        return [base_lr * self.last_epoch / (self.total_iters + 1e-8) for base_lr in self.base_lrs]
+
+def most_recent_folder(net_weights, fmt):
+    """
+        return most recent created folder under net_weights
+        if no none-empty folder were found, return empty folder
+    """
+    # get subfolders in net_weights
+    folders = os.listdir(net_weights)
+
+    # filter out empty folders
+    folders = [f for f in folders if len(os.listdir(os.path.join(net_weights, f)))]
+    if len(folders) == 0:
+        return ''
+
+    # sort folders by folder created time
+    folders = sorted(folders, key=lambda f: datetime.datetime.strptime(f, fmt))
+    return folders[-1]
+
+
+
+if __name__ == "__main__":
+    pass
